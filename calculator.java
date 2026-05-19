40596eeb348e69766f92b51941fcb96dc9512798
vladimir-mine <133777085+vladimir-mine@users.noreply.github.com>
2026-05-19 10:31:25 +0300

    Update calculator.java
    comments

--- calculator.java
+++ calculator.java
@@ -1,16 +1,16 @@
 public class Calculator {
 
-    public double add(double a, double b) {
+    public double add(double a, double b) { // 1111111
         return a + b;
     }
 
     public double subtract(double a, double b) {
-        return a - b;
+        return a - b; // 22222222
     }
 
     public double calculateAverage(double[] numbers) {
         if (numbers == null || numbers.length == 0) {
-            throw new IllegalArgumentException("Array must not be null or empty");
+            throw new IllegalArgumentException("Array must not be null or empty"); // 22131231231
         }
 
         double sum = 0;
